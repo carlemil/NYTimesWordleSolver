cd8050105e9218cf2b38f2680523cc79d217cc84
Carl-Emil Kjellstrand <erbsman@gmail.com>
2022-03-14 23:43:45 +0100

    attempting to rank results.

--- src/com/company/Main.kt
+++ src/com/company/Main.kt
@@ -7,9 +7,9 @@ import kotlin.jvm.JvmStatic
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
-        val notInUse = "aonirpy".toCharArray().toSet()
-        val wrongPlace = listOf("te", "e", "m", "et", "e")
-        val rightPlace = ".m...".toRegex()
+        val notInUse = "_".toCharArray().toSet()
+        val wrongPlace = listOf("_", "_", "_", "_", "_")
+        val rightPlace = ".....".toRegex()
 
         val inputStream: InputStream = File("corncob_lowercase_5_char_words.txt").inputStream()
         var fullList: MutableList<String> = arrayListOf()
@@ -26,13 +26,20 @@ object Main {
         }
         println("Not in use: " + notInUseFiltered.size + ", " + notInUseFiltered)
 
-        val wrongPlaceRegEx =
-            ("[" + wrongPlace[0] + "]....||.[" + wrongPlace[1] + "]...||..[" + wrongPlace[2] + "]..||...[" + wrongPlace[3] + "].||....[" + wrongPlace[4] + "]"
-                    ).toRegex()
+        val wrongPlaceRegEx = ("[" +
+                wrongPlace[0] + "]....||.[" +
+                wrongPlace[1] + "]...||..[" +
+                wrongPlace[2] + "]..||...[" +
+                wrongPlace[3] + "].||....[" +
+                wrongPlace[4] + "]").toRegex()
         val wrongPlaceFiltered = notInUseFiltered.filter { word ->
             !wrongPlaceRegEx.matches(word)
-
+        }.filter { word ->
+            wrongPlace.fold(true) { r, t ->
+                r && (t == "_" || t.toRegex().find(word)?.value != null)
+            }
         }
+
         println("Wrong place: " + wrongPlaceFiltered.size + ", " + wrongPlaceFiltered)
 
         val rightPlaceFiltered = wrongPlaceFiltered.filter { word ->
@@ -40,5 +47,24 @@ object Main {
         }
         println("Right place: " + rightPlaceFiltered.size + ", " + rightPlaceFiltered)
 
+        val rankedSortedList = rankList(rightPlaceFiltered)
+        println("Ranked: $rankedSortedList")
+    }
+
+    private fun rankList(wordsToRank: List<String>): List<String> {
+        var charMap = mutableMapOf<Char, Int>()
+        wordsToRank.forEach { word ->
+            word.toCharArray().forEach { c ->
+                charMap[c] = 1 + (charMap[c] ?: 0)
+            }
+        }
+        var rankedList = wordsToRank.map { word ->
+            val rank = word.toCharArray().toSet().toCharArray().fold(0) { r, t -> r + (charMap[t] ?: 0) }
+            Pair(word, rank)
+        }
+        val rankedSortedList = rankedList.sortedByDescending { t -> t.second }
+        return rankedSortedList.map { p ->
+            p.first
+        }
     }
 }
